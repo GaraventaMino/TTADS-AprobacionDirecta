e4831f711c5245fb8e2d14948ecb94de6175af1e
Bauti Garaventa <bautistagaraventa@hotmail.com>
2017-11-16 22:03:49 -0300

    agregado de imagenes a la BD

--- Backend/models/estadio.js
+++ Backend/models/estadio.js
@@ -3,7 +3,8 @@ var mongoose =require('mongoose');
 var estadioSchema= new mongoose.Schema({
   nombre: {type: String, required: true, unique: true},
   direccion: {type: String},
-  equipo: {type: mongoose.Schema.Types.ObjectId, ref: 'equipo'}
+  equipo: {type: mongoose.Schema.Types.ObjectId, ref: 'equipo'},
+  imagen: {type: String}
 },);
 
 mongoose.model('estadio', estadioSchema);
