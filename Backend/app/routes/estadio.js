81bb76b1efbf8dc8febcdbd9f40e7f1f310ee086
Bauti Garaventa <bautistagaraventa@hotmail.com>
2017-11-27 23:07:44 -0300

    estadio finalizado

--- Backend/app/routes/estadio.js
+++ Backend/app/routes/estadio.js
@@ -7,12 +7,10 @@ var router=require('express').Router()
 //GET ALL
 router.get('/', (req, res, next) => {
   Estadio.find().
-  populate({
-    path: 'equipo',
-    select: 'nombre tecnico'}).
+  populate('equipo').
   exec(function (err, result) {
     if (err) {
-      res.status(500).send(err);
+      res.send(err);
     }
     else if (result.length != 0) {
       res.json(result);
@@ -29,7 +27,7 @@ router.get('/:id', (req, res, next) => {
   populate('equipo').
   exec(function (err, result) {
     if (err) {
-      res.status(500).send(err);
+      res.send(err);
     } 
     if(result.length != 0) {
       res.json(result);
@@ -64,19 +62,18 @@ router.post('/', (req, res, next) => {
 router.put('/:id', (req, res, next) => {
   Estadio.findOne({_id: req.params.id}, function (err, result) {
     if (err) {
-      res.status(500).send(err);
+      res.send(err);
     } 
     else if (result) {
       result.nombre = req.body.nombre || result.nombre;
       result.direccion = req.body.direccion || result.direccion;
-      result.equipo = req.body.equipo || result.equipo;
       result.imagen = req.body.imagen || result.imagen;
       result.save((err, result) => {
         if(err) {
-          res.status(500).send(err)
+          res.send(err)
         }
         else {
-          res.status(200).send(result);
+          res.send("Estadio modificado con éxito");
         }
       });
     }
@@ -87,18 +84,56 @@ router.put('/:id', (req, res, next) => {
 });
 
 //DELETE ONE
+/* 
+El método se encarga de borrar el estadio y de borrarselo al equipo que lo contenga.
+*/
 router.delete('/:id', (req, res, next) => {
   Estadio.findOne({_id: req.params.id}, function (err, result) {
     if (err) {
-      res.status(500).send(err);
+      res.send(err);
     }
-    else if(result) {
-      result.remove((err, deleteEstadio) => {
+    else if(result != null) {
+      Equipo.find().
+      populate('estadios').
+      exec((err, eq) => {
         if(err) {
-          res.status(500).send(err);
+          res.send(err);
         }
-        res.status(200).send(deleteEstadio);
-      })
+        else if (eq.length != 0) {
+          for(var i = 0; i < eq.length; i++) {
+            for(var j = 0; j < eq[i].estadios.length; j++) {
+              if(eq[i].estadios[j]._id == result._id) {
+                Equipo.findOne({_id: eq[i]._id}).
+                populate('estadios').
+                exec((err, eq1) => {
+                  if(err) {
+                    res.send(err);
+                  }
+                  else if (eq1 != null) {
+                    var removed = eq1.estadios.splice(j, 1);
+                    eq1.save((err, guardado) => {
+                      if(err) {
+                        res.send(err);
+                      }
+                    });
+                  }
+                  else {
+                    res.send("Error al buscar el Equipo que contiene a este estadio");
+                  }
+                });
+              }
+            }
+          }
+        }
+        result.remove((err) => {
+          if(err) {
+            res.send(err);
+          }
+          else {
+            res.send("Estadio borrado con éxito");
+          }
+        });
+      });
     }
     else {
       res.send("No existe ese estadio");
